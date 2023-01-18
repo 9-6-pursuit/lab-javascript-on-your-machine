ac8ba549067a7b51e758dc83de801627037055cf
Judy <judychue@pursuit.org>
2023-01-17 22:57:47 -0500

    Calculator Functions

--- index.js
+++ index.js
@@ -4,7 +4,40 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let operation = process.argv[2]
+    let num = process.argv
+    let element = Number(process.argv[3])
+    
+    if (operation === undefined){
+        return "No operation provided..."
+    }
+    if (process.argv[3] === undefined){
+        return "No numbers provided..."
+    }
+
+    if (process.argv[2] === "plus") {
+        if (typeof (element) == "number") {
+            for (let index = 4; index < num.length; index++) {
+
+                element += Number(num[index]);
+                
+            }
+        } 
+
+    } else if (process.argv[2] === "minus"){
+        if (typeof (element) == "number") {
+            for (let index = 4; index < num.length; index++) {
+
+                element -= Number(num[index]);
+                
+            }
+        } 
+    } else if (process.argv[2] != "minus" && process.argv[2] != "plus" ){
+        element = "Invalid operation: modulo"
+    } 
+    return element
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
