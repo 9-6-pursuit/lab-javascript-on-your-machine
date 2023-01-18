c09a6488ec950dbbd0bbea6cabbbaaa511d4bee9
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-01-17 22:01:36 -0500

    I finished the lab.

--- index.js
+++ index.js
@@ -4,7 +4,36 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+let word = process.argv[2];
+let number = Number(process.argv[3])
+
+
+if (!word) {
+    return "No operation provided..."
+}
+if (process.argv.length < 4){
+    return "No numbers provided..."
+}
+
+if (word === "plus" ){
+for (let i = 4; i < process.argv.length; i++){ 
+    number += Number(process.argv[i])
+}
+}
+
+
+else if (word === "minus"){
+    for (let i = 4; i < process.argv.length; i++){
+        number -= Number(process.argv[i]);
+    }
+}
+else {
+    return "Invalid operation: modulo"
+}
+return number
+
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
