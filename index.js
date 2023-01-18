0417c60bb871b202a40ccef403bcc003228ae1cc
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-01-17 22:21:10 -0500

    added calculator function

--- index.js
+++ index.js
@@ -4,7 +4,56 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+//console.log(process.argv)
+//function calculator() {
+//     let operation = process.argv[2]
+//     let number =  Number(process.argv[3])
+//     if (operation === "plus"){
+//     for (let i = 4; i < process.argv.length; i++) {
+//         number += Number(process.argv[i])
+        
+//     }
+     
+//     }else if (operation === "minus"){
+//         for (let i = 4; i < process.argv.length; i++) {
+//             number -= Number(process.argv[i])
+            
+//     } 
+// } else {
+//  return process.argv.push(number)
+// }
+// return number
+// }
+function calculator() {
+    let operation = process.argv[2]
+    let number = Number(process.argv[3]);
 
+    if(!operation){
+        return "No operation provided..."
+    }
+    if(process.argv.length< 4){
+        return "No numbers provided..."
+    }
+if(operation === "plus"){
+    for( let i = 4; i < process.argv.length; i++){
+        number += Number(process.argv[i])
+    } 
+} else if( operation === "minus"){ 
+    for( let i = 4; i < process.argv.length; i++){
+    number -= Number(process.argv[i])
+} 
+
+} else {
+    return "Invalid operation: modulo"
+}
+return number
+}
+
+
+
+
+
+
+//console.log(calculator())
 // Don't change anything below this line.
 module.exports = calculator;
